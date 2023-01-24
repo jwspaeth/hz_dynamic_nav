e543e7f9f9995fc827325b0561852ecc5dada6c3
Will Spaeth <jwspaeth0@gmail.com>
2023-01-24 12:49:16 -0500

    Recent work

--- hz_dynamic_nav/run.py
+++ hz_dynamic_nav/run.py
@@ -1,6 +1,7 @@
 from habitat_baselines.run import main
 
-from hz_dynamic_nav.task import *
+# from hz_dynamic_nav.sims import *
+# from hz_dynamic_nav.tasks import *
 
 if __name__ == "__main__":
     main()
