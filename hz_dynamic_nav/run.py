5db5bbe8656ad2f44143f3b845ed9f899d54d7b2
Will Spaeth <jwspaeth0@gmail.com>
2023-01-24 16:14:20 -0500

    print

--- hz_dynamic_nav/run.py
+++ hz_dynamic_nav/run.py
@@ -2,5 +2,6 @@ from habitat.core.registry import registry
 from habitat_baselines.run import main
 
 TestNav = registry.get_task("TestNav")
+print("TestNav: ", TestNav)
 if __name__ == "__main__":
     main()
